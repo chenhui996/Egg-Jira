562f32c84074264bec76207cb2db9be74ab0733d
cain <chen562450669@qq.com>
2021-03-16 21:53:33 +0800

    chore: add Generics —— useDebounce

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -24,7 +24,7 @@ export const useMount = (callback: () => void) => {
   }, []);
 };
 
-export const useDebounce = (value: unknown, delay?: number) => {
+export const useDebounce = <V>(value: V, delay?: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
   useEffect(() => {
     const timeout = setTimeout(() => {
