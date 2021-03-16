9107647dad3589b4af4d78265f6b5505e353f455
cain <chen562450669@qq.com>
2021-03-16 13:56:24 +0800

    feat: add project-list

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import * as qs from "qs";
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+import { cleanObject } from "../../utils/index";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    // console.log(param);
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
+      async (response) => {
+        if (response.ok) {
+          setList(await response.json());
+        }
+      }
+    );
+  }, [param]);
+
+  useEffect(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  }, []);
+
+  return (
+    <div>
+      <SearchPanel
+        param={param}
+        setParam={setParam}
+        users={users}
+      ></SearchPanel>
+      <List list={list} users={users}></List>
+    </div>
+  );
+};
---
+++ src/screens/project-list/search-panel.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+export const SearchPanel = ({ param, setParam, users }) => {
+  return (
+    <form>
+      <div>
+        <input
+          type="text"
+          value={param.name}
+          onChange={(evt) =>
+            setParam({
+              ...param,
+              name: evt.target.value,
+            })
+          }
+        />
+        <select
+          value={param.personId}
+          onChange={(evt) => {
+            setParam({
+              ...param,
+              personId: evt.target.value,
+            });
+          }}
+        >
+          <option value="">负责人</option>
+          {users.map((user) => {
+            return (
+              <option key={user.id} value={user.id}>
+                {user.name}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </form>
+  );
+};
