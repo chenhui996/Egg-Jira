9d39b8f176818ce29a20e19236e2d3d73d59a392
cain <chen562450669@qq.com>
2021-03-29 14:06:09 +0800

    feat: loading and error handling of me API

--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,4 +1,7 @@
+import React from "react";
 import styled from "@emotion/styled";
+import { Spin, Typography } from "antd";
+import { DevTools } from "jira-dev-tool";
 
 interface RowProps {
   gap?: number | boolean;
@@ -24,3 +27,27 @@ export const Row = styled.div<RowProps>`
         : undefined};
   }
 `;
+
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FullPageLoading = () => {
+  return (
+    <FullPage>
+      <Spin size="large" />
+    </FullPage>
+  );
+};
+
+export const FullPageErrorFallback = ({ error }: { error: Error | null }) => {
+  return (
+    <FullPage>
+      <DevTools />
+      <Typography.Text type="danger">{error?.message}</Typography.Text>
+    </FullPage>
+  );
+};
--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -3,10 +3,19 @@ import * as auth from "auth-provider";
 import { User } from "screens/project-list/search-panel";
 import { http } from "utils/http";
 import { useMount } from "utils";
+import { useAsync } from "utils/use-async";
+import { FullPageLoading, FullPageErrorFallback } from "../components/lib";
 
 interface AuthForm {
   username: string;
   password: string;
+}
+
+interface AuthContextProps {
+  user: User | null;
+  login: (form: AuthForm) => Promise<void>;
+  register: (form: AuthForm) => Promise<void>;
+  logout: () => Promise<void>;
 }
 
 // 每次启动时查找 token 是否存在
@@ -20,19 +29,22 @@ const bootstrapUser = async () => {
   return user;
 };
 
-const AuthContext = React.createContext<
-  | {
-      user: User | null;
-      login: (form: AuthForm) => Promise<void>;
-      register: (form: AuthForm) => Promise<void>;
-      logout: () => Promise<void>;
-    }
-  | undefined
->(undefined);
+const AuthContext = React.createContext<AuthContextProps | undefined>(
+  undefined
+);
 AuthContext.displayName = "AuthContext";
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null);
+  // const [user, setUser] = useState<User | null>(null);
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData: setUser,
+  } = useAsync<User | null>();
 
   const login = (form: AuthForm) => {
     return auth.login(form).then((user) => setUser(user));
@@ -45,8 +57,17 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   };
 
   useMount(() => {
-    bootstrapUser().then(setUser);
+    run(bootstrapUser());
+    // bootstrapUser().then(setUser);
   });
+
+  if (isIdle || isLoading) {
+    return <FullPageLoading />;
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />;
+  }
 
   return (
     <AuthContext.Provider
